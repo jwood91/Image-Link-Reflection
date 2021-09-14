cc025f10924caa9312463a1b04c6b4d46bc4ece1
Jwood91 <Jwood4589@gmail.com>
2021-09-14 17:20:38 +0100

    commits reflection feedback

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const getImages = () => {
   axios.get(`https://picsum.photos/v2/list?page=${randomPage}&limit=100`)
     .then(response => {
       pickRandomImage(response.data);
-      console.log(response.data)
+      // console.log(response.data)
     });
   };
 
@@ -13,12 +13,12 @@ const getImages = () => {
 /* selects a random image from the data provided */
 function pickRandomImage(data){
     let randomImage = Math.floor(Math.random() * (data.length));
-    console.log(randomImage);
+    // console.log(randomImage);
     let displayImage = data[randomImage];
     let imageUrl = displayImage.download_url;
     let imageId = displayImage.id;
     let imageAuthor = displayImage.author;
-    console.log(displayImage);
+    // console.log(displayImage);
     $('#unsplash').attr('src', imageUrl);
     $('#unsplash').attr('value', imageId);
     $('#image-id').attr('value', imageId);
@@ -40,11 +40,11 @@ $(function() {
     $(`#scroll`).addClass('scroll-hide');
     if ($('#unsplash').length) {
         getImages()
-        console.log('image is there')
+        // console.log('image is there')
 
     } else {
           $('#image-area').append($("<img id='unsplash' src=''/>"));
-          console.log('image is not there')
+          // console.log('image is not there')
           getImages()
     };
 
@@ -56,7 +56,7 @@ $(function() {
 
 function toggleLink() {
     $(`#input-outer`).toggleClass('active');
-    console.log('linked');
+    // console.log('linked');
 }
 
 function hideEmailInput() {
@@ -87,16 +87,21 @@ function linkImage() {
       id: $(`#unsplash`).attr("value")
     };
 
+    if (currentImageID == " ") {
+      // console.log("image not found")
+      return;
+    }
+
     currentImage = [currentImageData]
-    console.log(currentImage)
+    // console.log(currentImage)
 
     let validEmail = false;
     if (emailInput.match(regex)) {
-      console.log('email validated');
+      // console.log('email validated');
       validEmail = true;
 
     } else {
-      console.log('email invalid');
+      // console.log('email invalid');
       if ($("#email-error").hasClass("fadeout")) {
           $("#email-error").removeClass("fadeout").addClass("fadein");
           setTimeout(function () {
@@ -115,7 +120,7 @@ function linkImage() {
     if (validEmail) {
         for (let item in linkedEmails) {
           if (item == emailInput) {
-          console.log("email in list");
+          // console.log("email in list");
           emailLinkedAlready = true;
           break
 
@@ -125,38 +130,38 @@ function linkImage() {
 
           for (let item in linkedEmails[emailInput]) {
             if (currentImageID == linkedEmails[emailInput][item]["id"]) {
-                console.log('image already linked to email')
+                // console.log('image already linked to email')
                 imageLinkedAlready = true;
                 break;
             }
           };
 
           if (imageLinkedAlready) {
-                console.log(linkedEmails);
+                // console.log(linkedEmails);
                 $(`#link-failed`).removeClass('hide-message').addClass('display-message');
                 setTimeout(() => {  $(`#link-failed`).removeClass('display-message').addClass('hide-message'); }, 2500);
               return;
             };
             $(`#link-success`).removeClass('hide-message').addClass('display-message');
             setTimeout(() => {  $(`#link-success`).removeClass('display-message').addClass('hide-message'); }, 2500);
-          console.log("email is already linked item added to object")
+          // console.log("email is already linked item added to object")
           linkedEmails[emailInput].push(currentImageData);
 
-          console.log(linkedEmails)
+          // console.log(linkedEmails)
 
         } else {
 
-            console.log(currentImage)
+            // console.log(currentImage)
             linkedEmails[emailInput] = currentImage;
             $(`#link-success`).removeClass('hide-message').addClass('display-message');
             setTimeout(() => {  $(`#link-success`).removeClass('display-message').addClass('hide-message'); }, 2500);
-            console.log(linkedEmails[emailInput])
+            // console.log(linkedEmails[emailInput])
             $('#email-list').empty();
             listEmail();
       };
     };
 
-  console.log(linkedEmails)
+  // console.log(linkedEmails)
     };
 
 
@@ -175,12 +180,12 @@ function displayLinked() {
     $('#my-button').click();
     $('#image-id').attr("value", ' ');
     $('#author-name').attr("value", ' ');
-    console.log("window empty")
+    // console.log("window empty")
     $(`#scroll`).removeClass('scroll-hide');
     let email = event.target.id
-    console.log(email)
-    console.log(linkedEmails)
-    console.log(linkedEmails[email].length)
+    // console.log(email)
+    // console.log(linkedEmails)
+    // console.log(linkedEmails[email].length)
     if (linkedEmails[email].length == 1) {
       for (const item in linkedEmails[email]) {
           $(`#image-area`).append($("<img  src='" + linkedEmails[email][item]["download_url"] +"' id='" + linkedEmails[email][item]["id"] + "' class='base-image'>"))
